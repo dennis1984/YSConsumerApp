697b586bf4dd246cc2da5f29759a06b62397c2c8
dennis1984 <year2005706@163.com>
2017-06-15 15:47:11 +0800

    settings设置

--- YSConsumerApp/settings.py
+++ YSConsumerApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2kpeanl_l@*1-coeuk&6i(w8)54svrw)%ohd2n*p6kw2gs-m0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
