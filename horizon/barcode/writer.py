933ba70aea41fe4d8a7e2bdfd2ec749355760a42
dennis1984 <year2005706@163.com>
2017-08-08 14:09:03 +0800

    条形码显示优化

--- horizon/barcode/writer.py
+++ horizon/barcode/writer.py
@@ -10,5 +10,5 @@ class ImageWriter(writer.ImageWriter):
         font = ImageFont.truetype(FONT, self.font_size * 3)
         width, height = font.getsize(self.text)
         pos = (mm2px(xpos, self.dpi) - width // 3,
-               mm2px(ypos, self.dpi) - height // 4)
+               mm2px(ypos, self.dpi) - height)
         self._draw.text(pos, self.text, font=font, fill=self.foreground)
